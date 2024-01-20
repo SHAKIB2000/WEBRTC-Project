dc0a16375c92ced3aa0e98636c1e2525962f1028
Md. Mansurul Hakim Shakib <mansurulhakim2000@gmail.com>
2024-01-21 01:11:34 +0600

    Project Creation

---
+++ src/WEBRTC/WEBRTC.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WEBRTC.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
