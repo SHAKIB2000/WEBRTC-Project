8031cf87fcc506df3dc190af16044ae9ab1275d2
Md. Mansurul Hakim Shakib <mansurulhakim2000@gmail.com>
2024-01-21 11:45:05 +0600

    Peer Connection

--- src/WEBRTC/WEBRTC.Web/wwwroot/js/site.js
+++ src/WEBRTC/WEBRTC.Web/wwwroot/js/site.js
@@ -10,7 +10,7 @@ const localVideo = document.getElementById('localVideo');
 const muteButton = document.getElementById("muteButton");
 const cameraButton = document.getElementById("cameraButton");
 
-let mute = true;
+let mute = false;
 let camera = true;
 
 // Local stream that will be reproduced on the video.
@@ -25,13 +25,13 @@ function gotLocalMediaStream(mediaStream) {
 muteButton.addEventListener("click", event => {
     if (mute) {
         mute = false;
-        muteButton.textContent = "Mute yourself";
+        muteButton.textContent = "Mute";
         localStream.getAudioTracks().forEach(track => {
                 track.enabled = true;
             })
     } else {
         mute = true;
-        muteButton.textContent = "Unmute yourself";
+        muteButton.textContent = "Unmute";
         localStream.getAudiotracks().foreach(track => {
                 track.enabled = false;
             })
@@ -41,14 +41,14 @@ muteButton.addEventListener("click", event => {
 cameraButton.addEventListener("click", event => {
     if (camera) {
         camera = false;
-        cameraButton.textContent = "Turn on camera";
+        cameraButton.textContent = "Camera On";
         localStream.getVideoTracks().forEach(track => {
                 track.enabled = false;
             })
 
     } else {
         camera = true;
-        cameraButton.textContent = "Turn off camera";
+        cameraButton.textContent = "Camera Off";
         localStream.getVideoTracks().forEach(track => {
                 track.enabled = true;
             })
@@ -68,3 +68,72 @@ navigator.mediaDevices.getUserMedia(mediaStreamConstraints)
 
 
 
+// Set up an asynchronous communication channel that will be
+// used during the peer connection setup
+const signalingChannel = new SignalingChannel(remoteClientId);
+signalingChannel.addEventListener('message', message => {
+    // New message from remote client received
+});
+
+// Send an asynchronous message to the remote client
+signalingChannel.send('Hello!');
+
+async function makeCall() {
+    const configuration = { 'iceServers': [{ 'urls': 'stun:stun.l.google.com:19302' }] }
+    const peerConnection = new RTCPeerConnection(configuration);
+    signalingChannel.addEventListener('message', async message => {
+        if (message.answer) {
+            const remoteDesc = new RTCSessionDescription(message.answer);
+            await peerConnection.setRemoteDescription(remoteDesc);
+        }
+    });
+    const offer = await peerConnection.createOffer();
+    await peerConnection.setLocalDescription(offer);
+    signalingChannel.send({ 'offer': offer });
+}
+
+const peerConnection = new RTCPeerConnection(configuration);
+signalingChannel.addEventListener('message', async message => {
+    if (message.offer) {
+        peerConnection.setRemoteDescription(new RTCSessionDescription(message.offer));
+        const answer = await peerConnection.createAnswer();
+        await peerConnection.setLocalDescription(answer);
+        signalingChannel.send({ 'answer': answer });
+    }
+});
+
+// Listen for local ICE candidates on the local RTCPeerConnection
+peerConnection.addEventListener('icecandidate', event => {
+    if (event.candidate) {
+        signalingChannel.send({ 'new-ice-candidate': event.candidate });
+    }
+});
+
+// Listen for remote ICE candidates and add them to the local RTCPeerConnection
+signalingChannel.addEventListener('message', async message => {
+    if (message.iceCandidate) {
+        try {
+            await peerConnection.addIceCandidate(message.iceCandidate);
+        } catch (e) {
+            console.error('Error adding received ice candidate', e);
+        }
+    }
+});
+
+// Listen for connectionstatechange on the local RTCPeerConnection
+peerConnection.addEventListener('connectionstatechange', event => {
+    if (peerConnection.connectionState === 'connected') {
+        // Peers connected!
+    }
+});
+
+localStream.getTracks().forEach(track => {
+    peerConnection.addTrack(track, localStream);
+});
+
+const remoteVideo = document.getElementById('remoteVideo');
+
+peerConnection.addEventListener('track', async (event) => {
+    const [remoteStream] = event.streams;
+    remoteVideo.srcObject = remoteStream;
+});
